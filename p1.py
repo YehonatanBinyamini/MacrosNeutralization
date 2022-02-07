de8b7349a70d059e82fa8c792b0d87794853ef17
YehonatamBinyamini <yonile2106@gmail.com>
2022-02-08 01:26:45 +0200

    final version

--- p1.py
+++ p1.py
@@ -1,6 +1,9 @@
 from openpyxl import load_workbook
 import sys
 
+#the Excel file mast be in the same folder for get the path
+
+
 path ='./' + sys.argv[1]
 
 wb = load_workbook(filename=path, read_only=False, keep_vba=False)
